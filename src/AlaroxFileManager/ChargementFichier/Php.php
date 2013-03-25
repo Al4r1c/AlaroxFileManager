67c3d089b346d0c1812de460c809b40991435f56
yann <yann.kurdijaka@wanadoo.fr>
2013-03-25 14:18:32 +0100

    Il s'est supprimé tout seul lui

---
+++ src/AlaroxFileManager/ChargementFichier/Php.php
@@ -0,0 +1,14 @@
+<?php
+namespace AlaroxFileManager\ChargementFichier;
+
+class Php extends AbstractChargeurFichier
+{
+    /**
+     * @param string $locationFichier
+     * @return mixed
+     */
+    public function chargerFichier($locationFichier)
+    {
+        return include $locationFichier;
+    }
+}
